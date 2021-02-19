a73810b5a53436c0887fccd6140fc9eed0964204
valclassdevelop <developervalclass@gmail.com>
2021-02-19 13:58:44 +0700

    menghilangkan bug pada v-font dan mengacak kode DOMnavbar.js

--- property/DOMnavbar.js
+++ property/DOMnavbar.js
@@ -1,6 +1,1 @@
-document.querySelector('.v-btn-toggle input').addEventListener('click', () => {
-    document.querySelector('.v-navbar ul').classList.toggle('show')
-})
-document.querySelector('.v-btn-toggle input').addEventListener('click', () => {
-    document.querySelector('.v-navbar-fixed ul').classList.toggle('show')
-})
+document.querySelector(".v-btn-toggle input").addEventListener("click",()=>{document.querySelector(".v-navbar ul").classList.toggle("show")}),document.querySelector(".v-btn-toggle input").addEventListener("click",()=>{document.querySelector(".v-navbar-fixed ul").classList.toggle("show")});
